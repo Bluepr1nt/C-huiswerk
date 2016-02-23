f9ae7e981c776eea12b9e11c839c0a2d6d522639
Jeroentjuh99 <jeroenvanos2@gmail.com>
2016-02-23 17:08:41 +0100

    code

--- week 2/linked list/Header.h
+++ week 2/linked list/Header.h
@@ -1,11 +1,11 @@
 struct ListNode;
-typedef struct ListNode{
+typedef struct ListNode {
 	struct ListNode *next;
 	struct ListNode *prev;
 	void *value;
 } ListNode;
 
-typedef struct List{
+typedef struct List {
 	int count;
 	ListNode *first;
 	ListNode *last;
@@ -14,8 +14,6 @@ typedef struct List{
 #define LIST_FOREACH(list, S, M, V) ListNode *_node = NULL;\
     ListNode *V = NULL;\
     for(V = _node = list->S; _node != NULL; V = _node = _node->M)
-
-#endif
 
 void llist_clear(List *list);
 List *List_create();
--- week 2/linked list/main.c
+++ week 2/linked list/main.c
@@ -1,106 +1,111 @@
-#include "stdlib.h"
-#include "stdio.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include "header.h"
 
-void main(void){
+void main(void) {
 	List list = llist_create();
-	llist_add(list, "hallo\0");
-	llist_add(list, 341);
-	llist_add(list, "doei\0");
-	printf(llist_nrItems(list));
-	llist_show(list);
-	llist_remove(list, 341);
-	llist_show(list);
-	llist_clear(list);
+	llist_add(&list, "hallo\0");
+	llist_add(&list, 341);
+	llist_add(&list, "doei\0");
+	printf("" + llist_nrItems(&list));
+	llist_show(&list);
+	llist_remove(&list, 341);
+	llist_show(&list);
+	llist_clear(&list);
 }
 
-List *List_create(){
+List *List_create() {
 	return calloc(1, sizeof(List));
 }
 
-void llist_show(List *list){
-	LIST_FOREACH(list, first, next, cur){
+void llist_show(List *list) {
+	LIST_FOREACH(list, first, next, cur) {
 		printf(cur->value);
 	}
 }
 
-int llist_excist(List *list, void *item){
-	LIST_FOREACH(list, first, next, cur){
-		if(cur->value == item){
+int llist_excist(List *list, void *item) {
+	LIST_FOREACH(list, first, next, cur) {
+		if (cur->value == item) {
 			printf("Item excists");
-			return cur->count;
+			return 1;
 		}
 	}
 }
 
-void llist_remove(List *list, ListNode node){
-	LIST_FOREACH(list, first, next, cur){
-		if(cur == node){
-			List_remove(list, node);
+void llist_remove(List *list, void *item) {
+	LIST_FOREACH(list, first, next, cur) {
+		if (cur->value == item) {
+			List_remove(list, cur);
 		}
 	}
 }
 
-List llist_create(){
+List llist_create() {
 	List *l = List_create();
-	if(l == NULL){
+	if (l == NULL) {
 		printf("Derp");
-	} else{
-		return l;
+	}
+	else {
+		return *l;
 	}
 }
 
-void llist_add(List *list, void *value){
+void llist_add(List *list, void *value) {
 	ListNode *node = calloc(1, sizeof(ListNode));
 	check_mem(node);
 	node->value = value;
-	
-	if(list->last == NULL){
+
+	if (list->last == NULL) {
 		list->first = node;
 		list->last = node;
-	} else{
+	}
+	else {
 		list->last->next = node;
 		node->prev = list->last;
 		list->last = node;
 	}
 	list->count++;
-	
-	error: return;
+
+error: return;
 }
 
-void List_remove(List *list, ListNode *node){
+void List_remove(List *list, ListNode *node) {
 	void *result = NULL;
 	check(list->first && list->last, "List is empty");
 	check(node, "Node can't be NULL");
-	
-	if(node == list->first && node == list->last){
+
+	if (node == list->first && node == list->last) {
 		list->first = NULL;
 		list->last = NULL;
-	} else if(node == list->first){
+	}
+	else if (node == list->first) {
 		list->first = node->next;
 		check(list->first != NULL, "Invalid list: First = NULL");
 		list->first->prev = NULL;
-	} else if(node == list->last){
+	}
+	else if (node == list->last) {
 		list->last = node->prev;
 		check(list->last != NULL, "Invalid list: Next = NULL");
 		list->last->next = NULL;
-	} else{
+	}
+	else {
 		ListNode *after = node->next;
 		ListNode *before = node->prev;
 		after->prev = before;
 		before->next = after;
 	}
-	
+
 	list->count--;
 	result = node->value;
 	free(node);
-	
-	error: return result;
+
+error: return result;
 }
 
-void llist_clear(List *list){
-	LIST_FOREACH(list, first, next, cur){
-		if(cur->prev){
+void llist_clear(List *list) {
+	LIST_FOREACH(list, first, next, cur) {
+		if (cur->prev) {
 			free(cur->prev);
 		}
 	}
@@ -108,10 +113,11 @@ void llist_clear(List *list){
 	free(list);
 }
 
-int llist_nrItems(List *list){
-	if(list->last->count != NULL){
-		return list->last->count;
-	} else{
+int llist_nrItems(List *list) {
+	if (list->count != NULL) {
+		return list->count;
+	}
+	else {
 		return 0;
 	}
 }
