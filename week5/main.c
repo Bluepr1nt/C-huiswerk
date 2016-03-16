e3fdf8348b93cb1f0ccb99d82e212f98c269a31e
Jeroentjuh99 <jeroenvanos2@gmail.com>
2016-03-16 12:46:08 +0100

    week 5 af

--- week5/main.c
+++ week5/main.c
@@ -1,146 +1,63 @@
-/*
- * Copyright (C) 2009-2011 by egnite GmbH
- * Copyright (C) 2001-2004 by egnite Software GmbH
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holders nor the names of
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
- * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * For additional information see http://www.ethernut.de/
- */
-
 #include <dev/board.h>
-
-#include <dev/urom.h>
-
-#include <sys/version.h>
+#include <stdio.h>
+#include <io.h>
+ 
+#include <sys/thread.h>
 #include <sys/timer.h>
 #include <sys/confnet.h>
 #include <sys/socket.h>
-
+ 
 #include <arpa/inet.h>
-
+#include <net/route.h>
+#include <netinet/tcp.h>
+ 
 #include <pro/dhcp.h>
-#include <pro/httpd.h>
+#include <dev/nicrtl.h>
 
-#include <io.h>
-#include <errno.h>
-
-#include <cfg/arch.h>
-#include <cfg/uart.h>
-#include <dev/debug.h>
-#include <dev/null_ether.h>
-
-
-//Defines
-#define	DEV_DEBUG	devDebug0
-#define	DEV_DEBUG_NAME	"uart0"
-#define	DEV_UART0	devUart0
-#define	DEV_UART0_NAME	"uart0"
-#define	DEV_UART	DEV_UART0
-#define	DEV_UART_NAME	DEV_UART0_NAME
-#define	DEV_CONSOLE	DEV_UART
-#define	DEV_CONSOLE_NAME	DEV_UART_NAME
-#define	DEV_ETHER_NAME	"eth0"
-
+#include <string.h>
+ 
+THREAD(TelnetThread, arg)
+{
+    TCPSOCKET *my_sock = NULL;
+    char *text = "Hello user! If this wasn't just an example you could now enter commands!";
+ 
+    my_sock = NutTcpCreateSocket();
+    NutTcpAccept(my_sock, 23);
+ 
+    NutThreadCreate("telnetd", TelnetThread, NULL, 512);
+    NutTcpSend(my_sock, text, strlen(text));
+ 
+    NutTcpCloseSocket(my_sock);
+    NutThreadExit();
+ 
+    for (;;) {
+    }
+}
+ 
 int main(void)
 {
-    uint32_t baud = 115200;
-    TCPSOCKET *sock;
-    FILE *stream;
-
-    /*
-     * Initialize the console.
-     */
-    NutRegisterDevice(&DEV_CONSOLE, 0, 0);
-    freopen(DEV_CONSOLE_NAME, "w", stdout);
+    unsigned long baud = 115200;
+ 
+    NutRegisterDevice(&DEV_DEBUG, 0, 0);
+ 
+    freopen(DEV_DEBUG_NAME, "w", stdout);
     _ioctl(_fileno(stdout), UART_SETSPEED, &baud);
-
-    printf("\n\nSimple HTTP Daemon running on Nut/OS %s\n",
-           NutVersionString());
-
-    /*
-     * Initialize the network interface.
-     */
-    printf("Configure %s...", DEV_ETHER_NAME);
+ 
+    puts("\nNut/OS Dynamic Server Threads Demo");
+ 
     if (NutRegisterDevice(&DEV_ETHER, 0, 0)) {
-        puts("failed. Cannot register Ethernet device.");
-        for (;;);
+        puts("Registering " DEV_ETHER_NAME " failed.");
     }
-    if (NutDhcpIfConfig(DEV_ETHER_NAME, NULL, 60000)) {
-        puts("failed. Cannot configure network.\nUse editconf.");
-        for (;;);
+ 
+    else if (NutDhcpIfConfig(DEV_ETHER_NAME, NULL, 0)) {
+        puts("Configuring " DEV_ETHER_NAME " failed.");
     }
-    printf("%s ready\n", inet_ntoa(confnet.cdn_ip_addr));
-
-    /*
-     * Initialize the file system.
-     */
-    printf("Register UROM file system...");
-    if (NutRegisterDevice(&devUrom, 0, 0)) {
-        puts("failed.");
-        for (;;);
-    }
-    puts("OK.");
-
-    /*
-     * Now loop endless for connections.
-     */
+ 
+    printf("Network configured. Listening on IP %s, port 23.", inet_ntoa(confnet.cdn_ip_addr));
+    NutThreadCreate("telnetd", TelnetThread, NULL, 512);
+ 
     for (;;) {
-        /* Create a socket. */
-        sock = NutTcpCreateSocket();
-        if (sock == NULL) {
-            printf("Error %d creating socket.\n", errno);
-            NutSleep(1000);
-            continue;
-        }
-
-        /* Listen on port 80. NutTcpAccept() will block
-           until we get a connection from a client. */
-        printf("Listening...");
-        NutTcpAccept(sock, 80);
-        printf("Connected...");
-
-        /* Associate a binary stdio stream with the socket. */
-        stream = _fdopen((int) ((uintptr_t) sock), "r+b");
-        if (stream == NULL) {
-            printf("Error %d creating stream.\n", errno);
-        } else {
-            /* This API call saves us a lot of work. It will parse
-               the client's HTTP request, send the requested file. */
-            NutHttpProcessRequest(stream);
-
-            /* Destroy the associated stream. */
-            fclose(stream);
-        }
-
-        /* Close the socket. */
-        NutTcpCloseSocket(sock);
-        puts("Disconnected");
+        NutThreadYield();
     }
     return 0;
 }
